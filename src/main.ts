a2d50264867f761f785abcbe858a57cf4dd32fa1
inho <inho@gomicorp.com>
2022-05-05 13:43:17 +0900

    [feat] swagger 문서 추가

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import liquibaseRunner from './utils/liquibase-runner';
@@ -8,6 +9,15 @@ async function bootstrap() {
 
   // liquibase changelog 실행
   await liquibaseRunner.update(null);
+
+  // swagger 설치
+  const config = new DocumentBuilder()
+    .setTitle('simple ecommerce')
+    .setDescription('The simple ecommerce API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
