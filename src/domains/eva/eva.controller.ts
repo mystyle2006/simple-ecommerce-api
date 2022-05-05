4142620a82a8f42a011ed22dd0efc17158d3b61c
inho <inho@gomicorp.com>
2022-05-05 15:45:31 +0900

    [feat] 문서 추가

--- src/domains/eva/eva.controller.ts
+++ src/domains/eva/eva.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 
 import { CreateEvaDto } from './dto/create-eva.dto';
 import { UpdateEvaDto } from './dto/update-eva.dto';
@@ -26,6 +27,9 @@ export class EvaController {
     return this.evaService.findAll();
   }
 
+  @ApiOperation({
+    summary: '상점 별 사용자 정의 필드 목록 가져오기',
+  })
   @Get(':store_id')
   findEVA(@Param('store_id') store_id: number) {
     return this.evaService.findEVA(+store_id);
